c6def70e0f5b4c670eaa8e7c6fd22222ba492705
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-04-11 21:42:03 +0300

    fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -21,8 +21,10 @@ const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      const index = state.findIndex(contact => contact.id === action.payload);
-      state.splice(index, 1);
+      const index = state.items.findIndex(
+        contact => contact.id === action.payload
+      );
+      state.items.splice(index, 1);
     },
   },
 });
